d342634d3caa3d5961fed800e4161abe48c5cef2
Woody Lin <wancheng.lin001@umb.edu>
2025-08-14 19:06:42 -0400

    Update PopupComponent.jsx

--- react-demos/src/counter/PopupComponent.jsx
+++ react-demos/src/counter/PopupComponent.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import SimpleCounter from "./SimpleCounter";
 import AutoCounter from "./AutoCounter";
 import MaxCounter from "./MaxCounter";
+import Counters from "./Counters";
 
 const PopupComponent = () => {
     const [activeComponent, setActiveComponent] = useState("");
@@ -20,9 +21,14 @@ const PopupComponent = () => {
                 Max Counter
             </button>
 
+            <button onClick={() => setActiveComponent("counters")}>
+                Counters
+            </button>
+
             {activeComponent === "simpleCounter" && <SimpleCounter />}
             {activeComponent === "autoCounter" && <AutoCounter />}
             {activeComponent === "maxCounter" && <MaxCounter />}
+            {activeComponent === "counters" && <Counters />}
         </div>
     );
 };
