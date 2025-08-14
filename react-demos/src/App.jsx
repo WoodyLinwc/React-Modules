6857a492207e1a08ab4e6598abce62849f4945d5
Woody Lin <wancheng.lin001@umb.edu>
2025-08-14 15:01:08 -0400

    basic react router setup

--- react-demos/src/App.jsx
+++ react-demos/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Route, Routes, NavLink } from "react-router-dom";
+import Counter from "./pages/Counter";
+import FetchAPI from "./pages/FetchAPI";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+    return (
+        <div className="App">
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/">Home</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/counter">Counter</NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/fetch-api">FetchAPI</NavLink>
+                    </li>
+                </ul>
+            </nav>
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/counter" element={<Counter />}></Route>
+                <Route path="/fetch-api" element={<FetchAPI />}></Route>
+            </Routes>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ react-demos/src/pages/FetchAPI.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FetchAPI = () => {
+    return <div>FetchAPI</div>;
+};
+
+export default FetchAPI;
