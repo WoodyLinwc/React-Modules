10122fcf284c3092547c22ad7f5f1b522fa2e346
Woody Lin <wancheng.lin001@umb.edu>
2025-08-15 14:25:53 -0400

    Simple Fetch and Retry Fetch

--- react-demos/src/fetchapi/PopupComponent.jsx
+++ react-demos/src/fetchapi/PopupComponent.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+import SimpleFetch from "./SimpleFetch";
 
 const PopupComponent = () => {
-    return <div>PopupComponent</div>;
+    const [activeComponent, setActiveComponent] = useState("");
+
+    return (
+        <div>
+            <button onClick={() => setActiveComponent("simpleFetch")}>
+                Simple Fetch
+            </button>
+
+            {activeComponent === "simpleFetch" && <SimpleFetch />}
+        </div>
+    );
 };
 
 export default PopupComponent;
---
+++ react-demos/src/fetchapi/SimpleFetch.jsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from "react";
+
+const SimpleFetch = () => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    const url = "https://jsonplaceholder.typicode.com/users";
+
+    useEffect(() => {
+        const fetchData = async (url) => {
+            setError(null);
+            setLoading(true);
+            try {
+                const res = await fetch(url);
+                if (!res.ok) {
+                    // throw error
+                    throw new Error(`Error fetching data: ${res.status}`);
+                }
+                const data = await res.json();
+                setData(data);
+            } catch (error) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData(url);
+    }, [url]);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p style={{ color: "red" }}>{error}</p>;
+
+    return (
+        <div>
+            <h2>Simple Fetching</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* ? prevent null */}
+                    {data?.map(({ id, name, email }) => (
+                        <tr key={id}>
+                            <td>{name}</td>
+                            <td>{email}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default SimpleFetch;
