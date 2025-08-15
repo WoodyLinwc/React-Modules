5190edf05ac20c20c9f452ea2a9437c08a7999c5
Woody Lin <wancheng.lin001@umb.edu>
2025-08-15 17:11:32 -0400

    useFetch

--- react-demos/src/fetchapi/HookFetch.jsx
+++ react-demos/src/fetchapi/HookFetch.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import useFetch from "../hooks/useFetch";
 
 const HookFetch = () => {
-    return <div>HookFetch</div>;
+    const { data, error, loading } = useFetch(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+
+    if (loading) return <p>Loading</p>;
+    if (error) return <p style={{ color: "red" }}>{error}</p>;
+
+    return (
+        <div>
+            <h2>Hook Fetch</h2>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Street</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {data?.map(({ id, name, address }) => (
+                        <tr key={id}>
+                            <td>{name}</td>
+                            <td>
+                                {address.street}, {address.city}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
 
 export default HookFetch;
--- react-demos/src/hooks/useFetch.jsx
+++ react-demos/src/hooks/useFetch.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const useFetch = () => {
-    return <div>useFetch</div>;
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        // cancelling fetch when component unmount
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        const fetchData = async () => {
+            setError(null);
+            setLoading(true);
+            try {
+                const res = await fetch(url, { signal });
+                if (!res.ok) {
+                    throw new Error(`Error fetching: ${res.status}`);
+                }
+                const data = await res.json();
+                setData(data);
+            } catch (error) {
+                // Abort Error
+                if (error.name === "AbortError") return;
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+
+        return () => {
+            controller.abort();
+        };
+    }, [url]);
+
+    return { data, error, loading };
 };
 
 export default useFetch;
